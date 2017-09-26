3b8d01748bc4b17918ef8e9f0c2437fd962fcece
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2017-09-26 17:59:28 -0500

    first commit

---
+++ projects.js
@@ -0,0 +1,20 @@
+var projects =
+[
+{
+    'project name': 'Fresh Tomatoes',
+    'project image': 'images/fresh_tomatoes.jpg',
+    'project site': 'https://github.com/rugby8724/udacity_movie_project'
+},
+
+{
+    'project name': 'Wiki Viewer',
+    'project image': 'images/wiki_viewer.jpg',
+    'project site': 'https://codepen.io/rugby8724/full/LxVjqd/'
+},
+
+{
+    'project name': 'Random Quote Generator',
+    'project image': 'images/random_quote.jpg',
+    'project site': 'https://github.com/rugby8724/udacity_movie_project'
+}
+]
